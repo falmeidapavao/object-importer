abe728d116d477a5d8caf62d18b752183539b856
falmeidapavao <131671115+fredalmeidapavao@users.noreply.github.com>
2025-04-19 12:42:04 +0000

    update summary and data-preview UI

--- src/components/data-preview/data-preview.jsx
+++ src/components/data-preview/data-preview.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import PreviewTable from "../preview-table/preview-table";
 import { useApp } from "./../../contexts/AppContext";
-import { Alert } from "@mui/material";
+
+// Material
+import { Alert, Typography } from "@mui/material";
 
 function DataPreview() {
   const { fileData } = useApp();
 
-  const numOfMappedColumns = fileData.columns.filter(
+  const getNumOfMappedColumns = fileData.columns.filter(
     (col) => col.mappedTo !== null
   ).length;
 
   return (
     <>
+      <Typography variant="h6" color="secondary" sx={{ mb: 3 }}>
+        Here is a preview of your mapped data:
+      </Typography>
       <Alert severity="info" sx={{ mb: 3 }}>
-        {`${numOfMappedColumns} out of ${fileData.columns.length} file columns were mapped to system fields`}
+        {`${getNumOfMappedColumns} out of ${fileData.columns.length} file columns were mapped to system fields`}
       </Alert>
       <PreviewTable
         columns={fileData.columns}
--- src/components/summary/summary.jsx
+++ src/components/summary/summary.jsx
@@ -2,14 +2,21 @@ import React, { useEffect, useState } from "react";
 import { useApp } from "../../contexts/AppContext.jsx";
 import api from "./../../api/api.js";
 
+// Material
+import { Box, Fade, LinearProgress, Alert, Typography } from "@mui/material";
+import { Circle } from "@mui/icons-material";
+
 function Summary() {
   const [summary, setSummary] = useState({});
+  const [isLoadingSummary, setIsLoadingSummary] = useState(false);
   const { fileData } = useApp();
 
   useEffect(() => {
     // Call summary to get mapping results
     const getSummary = async () => {
       try {
+        setIsLoadingSummary(true);
+
         const mappingResponse = await fetch(api.confirm.url, {
           ...api.confirm.config,
           body: JSON.stringify(fileData),
@@ -29,6 +36,7 @@ function Summary() {
           error.message
         );
       } finally {
+        setIsLoadingSummary(false);
       }
     };
 
@@ -37,17 +45,40 @@ function Summary() {
 
   return (
     <>
-      <div>This is the summary step!</div>
-      <div>Imported rows: {summary.imported}</div>
-      <div>Failed rows: {summary.failed}</div>
-      <div>Errors: </div>
-      {(summary.errors || []).map((error, errorIndex) => (
-        <>
-          <div key={errorIndex}>
-            Row: {error.row}: {error.message}
-          </div>
-        </>
-      ))}
+      {isLoadingSummary ? (
+        <LinearProgress />
+      ) : (
+        <Fade in={!isLoadingSummary} timeout={2000}>
+          <Box>
+            <Typography variant="h6" color="secondary" sx={{ mb: 3 }}>
+              Here is a summary of your work:
+            </Typography>
+            <Alert severity="success" sx={{ mb: 1 }}>
+              Imported rows: {summary.imported}
+            </Alert>
+            <Alert severity="error">
+              Failed rows: {summary.failed}
+              <Box>
+                {(summary.errors || []).map((error, errorIndex) => (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      columnGap: 0.5,
+                    }}
+                    key={errorIndex}
+                  >
+                    <Circle sx={{ fontSize: "0.4rem" }} />
+                    <Box>
+                      Row: {error.row}: {error.message}
+                    </Box>
+                  </Box>
+                ))}
+              </Box>
+            </Alert>
+          </Box>
+        </Fade>
+      )}
     </>
   );
 }
